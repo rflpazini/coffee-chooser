70c4f9785c3436dc23e72620e1ed05bd2991be35
Rafael Pazini <rafael.pazini@pluto.tv>
2023-11-16 23:18:38 -0300

    Feature/search with query (#38)
    * refactor: create get by name
    
    * refactor: fix compose and improve search using query params instead of path params
    
    * refactor: fix pass for compose dev

--- pkg/api/coffee/register.go
+++ pkg/api/coffee/register.go
@@ -10,27 +10,25 @@ type healthCheckParams struct {
 	dig.In
 
 	*echo.Echo
-	BrewingGetHandler       echo.HandlerFunc `name:"Route.Handler.Brewing.Get"`
-	BrewingGetByNameHandler echo.HandlerFunc `name:"Route.Handler.Brewing.Get.ByName"`
-	BrewingPostHandler      echo.HandlerFunc `name:"Route.Handler.Brewing.Post"`
-	BrewingDeleteHandler    echo.HandlerFunc `name:"Route.Handler.Brewing.Delete"`
+	BrewingGetHandler    echo.HandlerFunc `name:"Route.Handler.Brewing.Get"`
+	BrewingPostHandler   echo.HandlerFunc `name:"Route.Handler.Brewing.Post"`
+	BrewingDeleteHandler echo.HandlerFunc `name:"Route.Handler.Brewing.Delete"`
 }
 
 func setupBrewingRoutes(p healthCheckParams) router.RouteGroup {
 	brewingRoutes := p.Echo.Group("/v1")
 
 	brewingRoutes.GET("/brewing", p.BrewingGetHandler)
-	brewingRoutes.GET("/brewing/:name", p.BrewingGetByNameHandler)
 
 	brewingRoutes.POST("/brewing", p.BrewingPostHandler)
 
-	brewingRoutes.DELETE("/brewing/:name", p.BrewingDeleteHandler)
+	brewingRoutes.DELETE("/brewing", p.BrewingDeleteHandler)
 
 	return router.RouteGroup{Group: brewingRoutes}
 }
 
 func Register(c *dig.Container, register func(...interface{}) error) error {
-	if err := c.Provide(makeGetAllRequest, dig.Name("Route.Handler.Brewing.Get")); err != nil {
+	if err := c.Provide(makeGetRequest, dig.Name("Route.Handler.Brewing.Get")); err != nil {
 		return err
 	}
 
@@ -39,10 +37,6 @@ func Register(c *dig.Container, register func(...interface{}) error) error {
 	}
 
 	if err := c.Provide(makeDeleteByNameRequest, dig.Name("Route.Handler.Brewing.Delete")); err != nil {
-		return err
-	}
-
-	if err := c.Provide(makeGetByName, dig.Name("Route.Handler.Brewing.Get.ByName")); err != nil {
 		return err
 	}
 
